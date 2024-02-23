9b449966dab6388d36d77e3d4b83779f87d97ae3
Michael Beckwith <tw2113@gmail.com>
2024-02-23 09:23:39 -0600

    attempt using eslint more

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+	extends: [
+		'plugin:@wordpress/eslint-plugin/recommended',
+		'plugin:eslint-comments/recommended',
+	],
+	parserOptions: {
+		ecmaVersion: 2021,
+	},
+	root: true,
+	env: {
+		browser: true,
+		es6: true,
+	},
+	rules: {
+		'@wordpress/no-global-event-listener': 0, // Disable. We don't use React-based components.
+		camelcase: 1,
+	},
+};
